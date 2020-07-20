4d8bdba26539e79567c54469aaf9fd36b6b66916
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-07-21 02:22:02 +0900

    :ambulance: fix Css

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 import express from 'express';
 import cons from 'consolidate';
 import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
-
-app.get('/', (req, res) => {
-    return res.render('map');
-})
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'templates')))
 
 app.get('/login', (req, res) => {
-    return res.status(200).sendFile(__dirname + '/templates/login.html');
+    res.sendFile(path.join(__dirname + '/templates/login.html'));
 })
 
 app.get('/signin', (req, res) => {
-    return res.status(200).sendFile(__dirname + '/templates/signin.html');
+    res.sendFile(path.join(__dirname + '/templates/login.html'));
 })
 
 const handleListening = () => {
