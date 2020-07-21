284362eff0e88558570e215084ef16f93495f0fa
Ubuntu <ubuntu@ip-172-31-36-138.ec2.internal>
2020-07-21 04:06:03 +0000

    :art: add pm2

--- controller/userController.js
+++ controller/userController.js
@@ -2,12 +2,10 @@ import route from '../routes';
 
 
 export const getLogin = (req, res) => {
-    console.log("LOGIN");
     return res.render("login.html");
 
 };
 
 export const getSignin = (req, res) => {
-    console.log("getSigniin");
-    return res.sendFile("signin.html");
+    return res.render("signin.html");
 };
