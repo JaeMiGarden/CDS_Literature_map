526eaa20c131c3f22b912fe5ca4b87a740ea72f7
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-08-05 10:42:27 +0900

    :sparkles: add verfiy Id
    중복체크 로직 삽입

--- controller/apiController.js
+++ controller/apiController.js
@@ -1,4 +1,5 @@
 import Board from '../database/model/Board';
+import User from '../database/model/User';
 
 export const apiPostBoard = async (req, res) => {
     if (req.isAuthenticated()) {
@@ -16,4 +17,20 @@ export const apiPostBoard = async (req, res) => {
 export const apiGetTotalBoard = async (req, res) => {
     const result = await Board.find({});
     return res.json(result);
+}
+
+export const apiGetUserExist = async (req, res) => {
+    const {
+        id
+    } = req.body;
+    const isUser = User.verifyId(id);
+    if (isUser === true) {
+        return res.json({
+            status: true
+        });
+    } else {
+        return res.json({
+            status: false
+        });
+    }
 }
--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import route from '../router/routes';
 
-import { apiPostBoard, apiGetTotalBoard } from '../controller/apiController';
+import { apiPostBoard, apiGetTotalBoard, apiGetUserExist } from '../controller/apiController';
 
 const apiRouter = express.Router();
 
 apiRouter.post(route.mapBoard, apiPostBoard);
 apiRouter.get(route.totalBoard, apiGetTotalBoard);
+apiRouter.get(route.verifyId, apiGetUserExist);
+
 export default apiRouter;
